723c0452856a5b2c6611bbfca31a531152963448
GeekinGH <111835669+GeekinGH@users.noreply.github.com>
2024-04-03 16:42:09 +0800

    Bug fixed

--- index.js
+++ index.js
@@ -78,34 +78,6 @@ app.use('/', async (req, res) => {
                 }],
             });
         }
-        
-        let response;
-        if (requestModel === 'gpt-3.5-turbo' || requestModel === 'gpt-4') {
-            const chatGPT = new ChatGPT(requestModel, requestAuthorization, requestBody.messages);
-            response = await chatGPT.handleResponse(await getResponse(chatGPT.url, 'POST', chatGPT.headers, chatGPT.body));
-        } else if (requestModel === 'gemini-pro' || requestModel === 'gemini') {
-            const gemini = new Gemini(requestModel, requestAuthorization, requestBody.messages);
-            response = await gemini.handleResponse(await getResponse(gemini.url, 'POST', gemini.headers, gemini.body));
-        } else if (requestModel === 'qwen-turbo' || requestModel === 'qwen-max') {
-            const qwen = new Qwen(requestModel, requestAuthorization, requestBody.messages);
-            response = await qwen.handleResponse(await getResponse(qwen.url, 'POST', qwen.headers, qwen.body));
-        } else if (requestModel === 'moonshot-v1-8k' || requestModel === 'moonshot-v1-32k') {
-            const kimi = new Kimi(requestModel, requestAuthorization, requestBody.messages);
-            response = await kimi.handleResponse(await getResponse(kimi.url, 'POST', kimi.headers, kimi.body));
-        } else if (requestModel === 'claude-3-opus-20240229') {
-            const claude3 = new Claude3(requestModel, requestAuthorization, requestBody.messages);
-            response = await claude3.handleResponse(await getResponse(claude3.url, 'POST', claude3.headers, claude3.body));
-        } else {
-            return res.json({
-                choices: [{
-                    message: {
-                        role: 'assistant',
-                        content: '不支持的 chat_model 类型',
-                    },
-                }],
-            });
-        }
-
         return res.json({
             choices: [{
                 message: {
